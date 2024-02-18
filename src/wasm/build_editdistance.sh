2679b7032dc155dfe440da0c15326b63a85a051d
James Seo <james@equiv.tech>
2024-02-18 00:22:53 -0800

    Add build scripts for WASM modules
    Also invoke them in package.json as a prebuild script.

---
+++ src/wasm/build_editdistance.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ue
+
+EMCC_PARAMS=(
+  '-sEXPORT_NAME=createEditDistanceModule'
+  '-sINITIAL_MEMORY=65536'
+  '-sSTACK_SIZE=40960'
+  '-sMALLOC=none'
+  '-sEXPORT_ES6'
+)
+
+# Append any user parameters.
+for param in "$@"; do
+  EMCC_PARAMS+=("${param}")
+done
+
+emcc editdistance.c -o editdistance.js "${EMCC_PARAMS[@]}" &&
+  mkdir -p ../../wasm &&
+  cp editdistance.js editdistance.wasm ../../wasm &&
+  rm -f editdistance.js editdistance.wasm
