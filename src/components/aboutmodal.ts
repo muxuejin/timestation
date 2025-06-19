c989bfa0a842fbf5c09955472a4a81f168357577
HuiDong Netwrok <muxuejin@outlook.com>
2025-06-20 01:18:10 +0800

    Update aboutmodal.ts

--- src/components/aboutmodal.ts
+++ src/components/aboutmodal.ts
@@ -102,27 +102,22 @@ export class AboutModal extends BaseElement {
                 >
                   <span class="flex flex-col gap-2">
                     <p>
-                      <span class="font-semibold">Time Station Emulator</span>
-                      emulates radio time signal broadcasts from
+                      <span class="font-semibold">时间站模拟器</span>
+                      模拟来自
                       <span class="font-semibold">BPC</span>,
                       <span class="font-semibold">DCF77</span>,
                       <span class="font-semibold">JJY</span>,
-                      <span class="font-semibold">MSF</span>, and
-                      <span class="font-semibold">WWVB</span>.
+                      <span class="font-semibold">MSF</span>,
+                      <span class="font-semibold">WWVB</span>的无线电时间信号广播.
                     </p>
                     <p>
-                      The vast majority of all radio-controlled clocks and
-                      watches (often marketed as &ldquo;atomic clocks&rdquo;)
-                      rely on one of these five stations. However, their
-                      broadcasts are limited in geographic range and notoriously
-                      prone to interference in urban areas, so many such clocks
-                      end up never actually using their self-setting
-                      functionality.
+                      绝大多数无线电控制钟表 (通常被称为 &ldquo;原子钟s&rdquo;)都依赖于这五个电台之一. 
+                      然而,它们的广播在地理范围上受到限制,而且众所周知,在城市地区容易受到干扰,
+                      因此许多这样的时钟最终从未真正使用过它们的自设置功能.
                     </p>
                     <p>
-                      <span class="font-semibold">Time Station Emulator</span>
-                      may allow setting such clocks when/where a suitable signal
-                      is not otherwise available.
+                      <span class="font-semibold">时间站模拟器</span>
+                      可以在没有合适信号的情况下设置此类时钟.
                     </p>
                   </span>
                 </div>
@@ -135,34 +130,31 @@ export class AboutModal extends BaseElement {
                   @change=${this.#changeSection}
                 />
                 <div class="collapse-title pl-0 text-lg font-bold">
-                  Quick Start
+                  快速入门
                 </div>
                 <div
                   class="${overflowY[1]} collapse-content text-pretty text-sm sm:text-base [@media(min-height:600px)]:max-h-[calc(100dvh-23rem)]"
                 >
                   <span class="flex flex-col gap-2">
                     <p>
-                      <span class="font-semibold">Time Station Emulator</span>
-                      works best with a
-                      <span class="font-semibold">built-in speaker</span> of a
-                      <span class="font-semibold">phone or tablet</span>.
+                      <span class="font-semibold">时间站模拟器</span>
+                      与
+                      <span class="font-semibold">手机或平板电脑</span>的
+                      <span class="font-semibold">内置扬声器</span>配合使用效果最佳.
                     </p>
                     <ul class="list-decimal pl-6">
                       <li>
                         <span class="flex flex-col gap-2">
                           <p>
                             <span class="font-semibold">
-                              Choose emulator settings.
+                              选择模拟器设置.
                             </span>
                           </p>
                           <p>
-                            The most important setting is which time station to
-                            emulate. Certain settings are only available for
-                            certain stations.
+                            最重要的设置是模拟哪个时间站点. 某些设置仅适用于某些站点.
                           </p>
                           <p>
-                            Clocks (or watches) that support more than one
-                            station may prefer one of them over the others.
+                            支持多个站点的时钟 (或手表) 可能会优先选择其中一个站点.
                           </p>
                         </span>
                       </li>
@@ -170,19 +162,14 @@ export class AboutModal extends BaseElement {
                         <span class="flex flex-col gap-2">
                           <p>
                             <span class="font-semibold">
-                              Choose any clock settings and place the clock into
-                              sync mode.
+                              选择任意时钟设置并将时钟置于同步模式.
                             </span>
                           </p>
                           <p>
-                            If your clock has them, try to choose station and/or
-                            time zone settings that make sense for your
-                            location.
+                            如果您的时钟有这些设置, 请尝试选择适合您位置的站点和/或时区设置.
                           </p>
                           <p>
-                            Most clocks provide a way to force a synchronization
-                            attempt. You will probably have to navigate menus
-                            and/or press physical buttons.
+                            大多数时钟都提供强制同步的功能. 你可能需要浏览菜单和/或按下实体按钮.
                           </p>
                         </span>
                       </li>
@@ -190,20 +177,14 @@ export class AboutModal extends BaseElement {
                         <span class="flex flex-col gap-2">
                           <p>
                             <span class="font-semibold">
-                              Position the speaker as close as possible to the
-                              clock&rsquo;s antenna.
+                              将扬声器放置在尽可能靠近时钟天线的位置.
                             </span>
                           </p>
                           <p>
-                            The transmission range is quite short, so
-                            positioning is crucial. Some experimentation will
-                            probably be required, especially if you&rsquo;re
-                            unsure where the antenna is.
+                            传输距离很短,所以定位至关重要.可能需要进行一些实验,尤其是在你不确定天线位置的情况下.
                           </p>
                           <p>
-                            The volume should be set so that the clock picks up
-                            the cleanest signal. Usually, this occurs at or near
-                            the maximum possible volume.
+                            应将音量设置为使时钟拾取最干净的信号.通常,音量应设置为最大或接近最大音量.
                           </p>
                           <div
                             class="alert alert-warning grid-flow-col items-start text-start"
@@ -215,25 +196,20 @@ export class AboutModal extends BaseElement {
                             <span class="flex min-w-0 flex-col gap-2">
                               <p>
                                 <span class="font-bold">
-                                  DO NOT PLACE YOUR EARS NEAR THE SPEAKER TO
-                                  DETERMINE VOLUME.
+                                  请勿将耳朵靠近扬声器来确定音量.
                                 </span>
                               </p>
-                              <p>Use a visual volume indicator instead.</p>
+                              <p>请改用可视音量指示器.</p>
                               <p>
-                                The generated waveform has full dynamic range,
-                                but is pitched high enough to be difficult to
-                                perceive.
+                                生成的波形具有全动态范围,但音调较高,难以察觉.
                               </p>
                               <p>
                                 <span class="font-bold">
-                                  Even if you &ldquo;can&rsquo;t hear
-                                  anything&rdquo;,
+                                  即使你 &ldquo;什么也听不到&rdquo;,
                                 </span>
-                                many common devices are capable of playing it
-                                back loud enough to potentially cause
+                                许多常见设备能够播放足够大的声音,可能会造成
                                 <span class="font-bold">
-                                  permanent hearing damage!
+                                  永久性听力损伤!
                                 </span>
                               </p>
                             </span>
@@ -244,13 +220,11 @@ export class AboutModal extends BaseElement {
                         <span class="flex flex-col gap-2">
                           <p>
                             <span class="font-semibold">
-                              Start transmitting and hold the speaker in
-                              position.
+                              开始发射并将扬声器保持在原位.
                             </span>
                           </p>
                           <p>
-                            If all goes well, the clock will set itself within
-                            three minutes.
+                            如果一切顺利,时钟将在三分钟内自行设置.
                           </p>
                         </span>
                       </li>
@@ -266,82 +240,68 @@ export class AboutModal extends BaseElement {
                   @change=${this.#changeSection}
                 />
                 <div class="collapse-title pl-0 text-lg font-bold">
-                  Calculating Offsets
+                  计算偏移量
                 </div>
                 <div
                   class="${overflowY[2]} collapse-content text-pretty text-sm sm:text-base [@media(min-height:600px)]:max-h-[calc(100dvh-23rem)]"
                 >
                   <span class="flex flex-col gap-2">
                     <p>
-                      Entering an offset changes the time encoded within the
-                      transmitted signal by up to one day in either direction.
-                      This is useful for correcting time zone differences as
-                      well as for setting a clock (or watch) a few minutes
-                      fast/slow.
+                      输入偏移量会使传输信号中编码的时间在任意方向上最多改变一天.
+                      这对于校正时区差异以及将时钟(或手表)调快/慢几分钟非常有用.
                     </p>
                     <p>
-                      You probably
-                      <span class="font-semibold">shouldn&rsquo;t</span> enter
-                      an offset when using this emulator to set your clock for
-                      the first time. Afterwards, the offset is easily
-                      calculated as the difference between the time that the
-                      clock should show and the time that the clock actually
-                      shows. If these are the same, no offset is needed.
+                      首次使用此模拟器设置时钟时,您可能<span class="font-semibold">不应该</span>输入偏移量. 
+                      之后,偏移量很容易计算出来,即时钟应显示的时间与实际显示的时间之间的差值.如果两者相同,则无需偏移.
                     </p>
-                    <p>Figuring out the offset in advance is often tricky:</p>
+                    <p>提前计算出偏移量通常很棘手:</p>
                     <ul class="list-disc pl-6">
                       <li>
-                        All five time stations broadcast the time relative to a
-                        different time zone.
+                        所有五个时间站都广播相对于不同时区的时间.
                       </li>
                       <li>
-                        <span class="font-semibold">DCF77</span> and
-                        <span class="font-semibold">MSF</span> change the
-                        transmitted hour value according to daylight saving
-                        time.
+                        <span class="font-semibold">DCF77</span> 和
+                        <span class="font-semibold">MSF</span> 根据夏令时改变传输的小时值.
                       </li>
                       <li>
-                        Many clocks apply their own offset when displaying the
-                        received time, as with clocks for the North American
-                        market that allow selection of a US time zone
-                        (Pacific&ndash;Eastern).
+                        许多时钟在显示接收时间时会应用自己的偏移量, 例如北美市场的时钟允许选择美国时区
+                        (太平洋东部).
                       </li>
                     </ul>
                     <p>
-                      Suppose you wish to set a watch from the UK that syncs to
-                      <span class="font-semibold">MSF</span> during a visit to
-                      <span class="font-semibold">Sydney, NSW, Australia</span>
-                      in the month of <span class="font-semibold">July</span>:
+                      假设您希望在<span class="font-semibold">7月</span>访问
+                      <span class="font-semibold">澳大利亚新南威尔士州悉尼</span>时设置一个来自英国的手表
+                      该手表与<span class="font-semibold">MSF</span>同步:
                     </p>
                     <ul class="list-disc pl-6">
                       <li>
-                        Sydney observes
-                        <span class="font-semibold">AEST (UTC+10:00)</span>.
+                        悉尼遵守
+                        <span class="font-semibold">澳大利亚东部标准时间 (UTC+10:00)</span>.
                       </li>
                       <li>
-                        MSF in the UK transmits a
-                        <span class="font-semibold">BST (UTC+01:00)</span> time.
+                        英国的 MSF 传输
+                        <span class="font-semibold">BST (UTC+01:00)</span> 时间.
                       </li>
                       <li>
-                        Sydney is 9 hours ahead of the UK, so the offset is
+                        悉尼比英国早9个小时, 因此偏移量为
                         <span class="font-semibold">+09:00</span>.
                       </li>
                     </ul>
                     <p>
-                      However, if your visit were to occur in
-                      <span class="font-semibold">January</span>:
+                      但是, 如果您在
+                      <span class="font-semibold">一月</span>访问:
                     </p>
                     <ul class="list-disc pl-6">
                       <li>
-                        Sydney observes
-                        <span class="font-semibold">AEDT (UTC+11:00)</span>.
+                        悉尼遵守
+                        <span class="font-semibold">澳大利亚东部标准时间 (UTC+11:00)</span>.
                       </li>
                       <li>
-                        MSF transmits a
-                        <span class="font-semibold">GMT (UTC+00:00)</span> time.
+                        MSF 传输 GMT
+                        <span class="font-semibold">GMT (UTC+00:00)</span> 时间.
                       </li>
                       <li>
-                        The offset is then
+                        偏移量为
                         <span class="font-semibold">+11:00</span>!
                       </li>
                     </ul>
@@ -356,32 +316,24 @@ export class AboutModal extends BaseElement {
                   @change=${this.#changeSection}
                 />
                 <div class="collapse-title pl-0 text-lg font-bold">
-                  Technical Details
+                  技术细节
                 </div>
                 <div
                   class="${overflowY[3]} collapse-content text-pretty text-sm sm:text-base [@media(min-height:600px)]:max-h-[calc(100dvh-23rem)]"
                 >
                   <span class="flex min-w-0 flex-col gap-2">
                     <p>
-                      <span class="font-semibold">Time Station Emulator</span>
-                      generates an audio waveform intentionally crafted to
-                      create, when played back through consumer-grade audio
-                      hardware, the right kind of RF noise to be mistaken for a
-                      time station broadcast.
+                      <span class="font-semibold">时间站模拟器</span>
+                      生成一种精心设计的音频波形,以便在通过消费级音频硬件播放时产生正确的RF噪声,从而被误认为是时间站广播.
                     </p>
                     <p>
-                      Specifically, given a fundamental carrier frequency used
-                      by a real time station, it generates and modulates the
-                      highest odd-numbered subharmonic that also falls below the
-                      Nyquist frequencies of common playback sample rates.
+                      具体来说,给定一个实时电台使用的基本载波频率,
+                      它会生成并调制最高的奇数次谐波,该次谐波也低于常见播放采样率的奈奎斯特频率.
                     </p>
                     <p>
-                      One of the higher-frequency harmonics inevitably created
-                      by any real-world DAC during playback will then be the
-                      original fundamental, which should leak to the environment
-                      as a short-range radio transmission via the ad-hoc antenna
-                      formed by the physical wires and circuit traces in the
-                      audio output path.
+                      任何现实世界的 DAC 在播放过程中不可避免地会产生高频谐波, 
+                      其中之一就是原始基波,它会通过音频输出路径中的物理线路和电路走线形成的临时天线，
+                      以短距离无线电传输的形式泄漏到环境中.
                     </p>
                     <div
                       class="alert grid-flow-col items-start text-start"
@@ -390,30 +342,26 @@ export class AboutModal extends BaseElement {
                       <span class="size-6 sm:size-8">${svgIcons.info}</span>
                       <span class="flex min-w-0 flex-col gap-2">
                         <p>
-                          Because it works by exploiting this leakage,
+                          由于它利用了这种泄漏,
                           <span class="font-semibold">
-                            Time Station Emulator
+                            时间站模拟器
                           </span>
-                          works best with a
-                          <span class="font-semibold">built-in speaker</span> of
-                          a <span class="font-semibold">phone or tablet</span>.
+                          最适合
+                          <span class="font-semibold">内置扬声器</span> 的
+                          a <span class="font-semibold">手机或平板电脑</span>.
                         </p>
                         <p>
-                          In some cases,
+                          在某些情况下,
                           <span class="font-semibold">
-                            wired headphones or earbuds
+                            有线耳机或耳塞
                           </span>
-                          may also be suitable.
+                          也可能适用.
                         </p>
                         <p>
-                          Higher-frequency harmonics are considered artifacts
-                          beyond the range of human hearing, so they are
-                          routinely suppressed by audio compression algorithms
-                          and better equipment.
+                          高频谐波被认为是超出人类听觉范围的人工制品，因此它们通常会被音频压缩算法和更好的设备抑制.
                         </p>
                         <p>
-                          Bluetooth devices and audiophile-grade equipment are
-                          therefore less likely to work.
+                          因此蓝牙设备和发烧级设备不太可能正常工作.
                         </p>
                       </span>
                     </div>
